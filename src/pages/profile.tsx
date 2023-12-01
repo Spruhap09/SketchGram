3ca3aca2be721bdd9d61ceac072a0c5fe3cbe9ae
Spruha Paradkar <spruhap@Spruhas-MacBook-Pro-2.local>
2023-12-01 15:30:31 -0500

    added some basic total stats information

--- src/pages/profile.tsx
+++ src/pages/profile.tsx
@@ -4,7 +4,8 @@ import { AuthContext } from "@/context/AuthContext";
 import { Avatar, Button, CardHeader, Typography } from "@material-tailwind/react";
 import { useRouter } from "next/router";
 import { useContext, useEffect, useState } from "react";
-import EditProfile from "./EditProfile";
+import EditProfile from "../components/EditProfile";
+import ProfileStats from "../components/ProfileStats";
 
 export default function Profile() {
 
@@ -20,9 +21,8 @@ export default function Profile() {
             </div>
             <div className="flex justify-between py-10 w-full">
                     <EditProfile/>
-                    <div className="flex-grow box-border h-32 w-32 p-4 border-4 rounded-lg">
-                        <Typography className="">there</Typography>
-                    </div>  
+                    <ProfileStats/>
+                    
             </div>
             <div>
                 <Typography variant="h4" className="text-center">Your Posts</Typography>
