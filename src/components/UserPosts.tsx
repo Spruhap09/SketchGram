4c5f1b088c332794ad220be1d3f23bd623b95605
YgEd <95714217+YgEd@users.noreply.github.com>
2023-12-02 13:19:46 -0500

    fixed profile page posts load, and updated getUserPostLimit with default value paramter

--- src/components/UserPosts.tsx
+++ src/components/UserPosts.tsx
@@ -1,32 +1,44 @@
-import { getUserPosts } from "@/firebase/functions";
+import { getUserPosts, getUserPostsLimit } from "@/firebase/functions";
 import { AuthContext } from "@/context/AuthContext";
 import Post from "@/components/Post";
 import { useContext, useEffect, useState } from "react";
 
 
 export default function UserPosts() {
-    const [posts, setPosts] = useState<string[]>([]);
+    const [posts, setPosts] = useState<any[] | null>(null);
     const user = useContext(AuthContext);
     
     useEffect(() => {
         const getPosts = async () => {
             if(user){
-                const postsData = await getUserPosts(user.uid);
-                const posts = postsData.map((post) => post.toString());
-                setPosts(posts);
+                const postsData = await getUserPostsLimit(user?.uid);
+                const posts = postsData;
+                setPosts(posts || null);
             }
         }
         getPosts()
+        {console.log(user)}
     }, [user])
-    
-    if(!user) return (<div>loading</div>)
+
+   
+   
     return (
         <div className="w-full h-full flex flex-row justify-evenly flex-wrap">
-            {posts.map((post) => {
-                return (
-                    <Post key={post} id={post} setPosts={setPosts}/>
-                )
-            })}
+          {/* if user exists but post doesn't exist, return a div saying no current posts */}
+          {user ? (
+            posts ? (
+              posts.map((post) => {
+                console.log(post);
+                return <Post key={post.post_id} id={post.post_id} setPosts={setPosts} />;
+              })
+            ) : (
+              <div>No current posts</div>
+            )
+          ) : (
+            <div>Loading</div>
+          )}
         </div>
-    )   
+      );
+      
 }
+
