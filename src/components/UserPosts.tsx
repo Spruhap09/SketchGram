4bf6f27add8cc884d30268424d22f08d9891a18e
YgEd <95714217+YgEd@users.noreply.github.com>
2023-12-02 15:40:04 -0500

    added Post Context to replace individual instances of setPosts useStates

--- src/components/UserPosts.tsx
+++ src/components/UserPosts.tsx
@@ -2,23 +2,42 @@ import { getUserPosts, getUserPostsLimit } from "@/firebase/functions";
 import { AuthContext } from "@/context/AuthContext";
 import Post from "@/components/Post";
 import { useContext, useEffect, useState } from "react";
+import { usePostsContext } from "@/context/PostsContext";
 
 
 export default function UserPosts() {
-    const [posts, setPosts] = useState<any[] | null>(null);
     const user = useContext(AuthContext);
+    const { state, dispatch } = usePostsContext();
     
     useEffect(() => {
+        console.log("ues effect triggered")
         const getPosts = async () => {
+
             if(user){
                 const postsData = await getUserPostsLimit(user?.uid);
                 const posts = postsData;
-                setPosts(posts || null);
-            }
+                if (posts) {
+                //console.log("before context " + JSON.stringify(posts));
+                const fetchPosts = async () => {
+                    try {
+                        posts.forEach((post) => {
+                            //console.log("added post: " + JSON.stringify(post));
+                            dispatch({ type: 'ADD_POST', payload: post });
+                        });
+                        //dispatch({type: 'CLEAR_POSTS'})
+                    } catch (error) {
+                      console.error('Error fetching posts:', error);
+                    }
+                }
+            fetchPosts();
+                }
         }
+    }
         getPosts()
-        {console.log(user)}
-    }, [user])
+        //{console.log(user)}
+       // {console.log(state)}
+    }, [user]);
+    
 
    
    
@@ -26,10 +45,11 @@ export default function UserPosts() {
         <div className="w-full h-full flex flex-row justify-evenly flex-wrap">
           {/* if user exists but post doesn't exist, return a div saying no current posts */}
           {user ? (
-            posts ? (
-              posts.map((post) => {
+            state.posts ? (
+                console.log(state.posts),
+              state.posts.map((post) => {
                 console.log(post);
-                return <Post key={post.post_id} id={post.post_id} setPosts={setPosts} />;
+                return <Post key={post.post_id} id={post.post_id} />;
               })
             ) : (
               <div>No current posts</div>
