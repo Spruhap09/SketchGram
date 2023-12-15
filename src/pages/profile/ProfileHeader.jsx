6ec2a667de339385919b671d541dc0635c6746e5
Stephanie Martinez <stephaniemartinez@stephanies-mbp.myfiosgateway.com>
2023-12-15 01:13:40 -0500

    More changes

--- src/pages/profile/ProfileHeader.jsx
+++ src/pages/profile/ProfileHeader.jsx
@@ -9,7 +9,7 @@ const ProfileHeader = ({profile, posts}) => {
         </div>
         <div>
         <div className="flex flex-col md:flex-row w-full justify-left items-center"> 
-          <h1 className="text-2xl font-bold mr-8">{profile.displayName ? profile.displayName : "Unknown"}</h1>
+          <h1 className="text-2xl font-bold mr-8">{profile?.displayName ? profile?.displayName : "Unknown"}</h1>
           <div>
             <button
               className="text-white ml-4 bg-black w-24 px-6 py-2 text-xs rounded shadow hover:bg-gray-800 focus:outline-none mt-4 ml-8 md:mt-0"
@@ -19,11 +19,11 @@ const ProfileHeader = ({profile, posts}) => {
           </div>
         </div>
           <div className="mt-4">
-            <span className="mr-4"><strong>{posts.length}</strong> Photos</span>
-            <span className="mr-4"><strong>{profile.followers}</strong> Followers</span>
-            <span><strong>{profile.following}</strong> Following</span>
+            <span className="mr-4"><strong>{posts?.length}</strong> Photos</span>
+            <span className="mr-4"><strong>{profile?.followers}</strong> Followers</span>
+            <span><strong>{profile?.following}</strong> Following</span>
           </div>
-          {profile.bio && <p className="text-gray-600 mt-4 md:mt-0 pt-3">{profile.bio}</p>}
+          {profile?.bio && <p className="text-gray-600 mt-4 md:mt-0 pt-3">{profile.bio}</p>}
         </div>
       </div>
     </div>
