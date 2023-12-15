99ed835aa993a8c33b17cc0463f57b66e8c199d1
Stephanie Martinez <stephaniemartinez@stephanies-mbp.myfiosgateway.com>
2023-12-15 00:20:45 -0500

    Made some updates

--- src/pages/profile/PhotoGrid.jsx
+++ src/pages/profile/PhotoGrid.jsx
@@ -1,11 +1,18 @@
-const PhotoGrid = ({ photos }) => {
-    return (
-      <div className="photo-grid">
-        {photos.map(photo => (
-          <img key={photo.id} src={photo.src} alt={photo.alt} />
+const PhotoGrid = ({ posts }) => {
+  return (
+    <div className="max-w-6xl mx-auto p-4">
+      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4">
+        {console.log("posts", posts)}
+        {posts.map((post, index) => (
+          <div key={index} className={`overflow-hidden ${index % 6 === 0 ? 'row-span-2' : ''} shadow-lg`}>
+            <img src={post.imageURL} alt={post.description} className="w-full object-cover" />
+          </div>
         ))}
       </div>
-    );
-  };
+    </div>
+  );
+};
+
+
 
 export default PhotoGrid
--- src/pages/profile/ProfileHeader.jsx
+++ src/pages/profile/ProfileHeader.jsx
@@ -1,19 +1,33 @@
 import noAvatar from 'public/noAvatar.jpeg'
 
-const ProfileHeader = ({profile}) => {
-    return (
-        <div className="profile-header">
-          <img src={noAvatar.src} alt={profile.name} className="profile-picture" />
-          <h1>{profile.name}</h1>
-          <h2>{profile.location}</h2>
-          <div className="stats">
-            <span>{profile.photos} Photos</span>
-            <span>{profile.followers} Followers</span>
-            <span>{profile.following} Following</span>
-          </div>
-          <button>Edit Profile</button>
+const ProfileHeader = ({profile, posts}) => {
+  return (
+    <div className="">
+      <div className="flex flex-col items-center md:items-start md:flex-row w-full justify-center">
+        <div className="mb-4 md:mb-0 md:mr-4">
+          <img src={noAvatar.src} className="w-40 h-30 rounded-lg mb-4" /> 
         </div>
-      );
-}
+        <div>
+        <div className="flex flex-col md:flex-row w-full justify-left items-center"> 
+          <h1 className="text-2xl font-bold mr-8">{profile.displayName ? profile.displayName : "Unknown"}</h1>
+          <div>
+            <button
+              className="text-white ml-4 bg-black w-24 px-6 py-2 text-xs rounded shadow hover:bg-gray-800 focus:outline-none mt-4 ml-8 md:mt-0"
+            >
+              EDIT PROFILE
+            </button>
+          </div>
+        </div>
+          <div className="mt-4">
+            <span className="mr-4"><strong>{posts.length}</strong> Photos</span>
+            <span className="mr-4"><strong>{profile.followers}</strong> Followers</span>
+            <span><strong>{profile.following}</strong> Following</span>
+          </div>
+          {profile.bio && <p className="text-gray-600 mt-4 md:mt-0 pt-3">{profile.bio}</p>}
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default ProfileHeader
