a4d6bf022b8ffb1d47806df2bc6b7c6c6ce5a8db
Stephanie Martinez <stephaniemartinez@stephanies-mbp.myfiosgateway.com>
2023-12-15 00:32:30 -0500

    Changed layout

--- src/pages/profile/PhotoGrid.jsx
+++ src/pages/profile/PhotoGrid.jsx
@@ -4,7 +4,7 @@ const PhotoGrid = ({ posts }) => {
       <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4">
         {console.log("posts", posts)}
         {posts.map((post, index) => (
-          <div key={index} className={`overflow-hidden ${index % 6 === 0 ? 'row-span-2' : ''} shadow-lg`}>
+          <div key={index} className={`overflow-hidden shadow-lg`}>
             <img src={post.imageURL} alt={post.description} className="w-full object-cover" />
           </div>
         ))}
