ad1b19ee1603d1a58dd78cf83390fa6ce89698bf
Stephanie Martinez <stephaniemartinez@stephanies-mbp.myfiosgateway.com>
2023-12-15 01:13:40 -0500

    More changes

--- src/pages/profile/PhotoGrid.jsx
+++ src/pages/profile/PhotoGrid.jsx
@@ -1,12 +1,16 @@
+import PhotoSquare from './PhotoSquare'
 const PhotoGrid = ({ posts }) => {
+  // const res = await fetch(`/api/image?url=${post?.imageURL}`);
+  // const data = await res.json();
   return (
     <div className="max-w-6xl mx-auto p-4">
       <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4">
-        {console.log("posts", posts)}
-        {posts.map((post, index) => (
-          <div key={index} className={`overflow-hidden shadow-lg`}>
-            <img src={post.imageURL} alt={post.description} className="w-full object-cover" />
-          </div>
+        {posts.map((p, index) => (
+          // <div key={index} className={`overflow-hidden shadow-lg`}>
+          //   <img src={post.imageURL} alt={post.description} className="w-full object-cover" />
+          // </div>
+          
+          <PhotoSquare post={p}/>
         ))}
       </div>
     </div>
