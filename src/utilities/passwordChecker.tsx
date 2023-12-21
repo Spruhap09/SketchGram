0137cd2c2b3582748fb6b81d041cef93fdfaf2c5
Spruha Paradkar <spruhap@Spruhas-MacBook-Pro-2.local>
2023-12-22 01:11:23 +0530

    added aditional special characters

--- src/utilities/passwordChecker.tsx
+++ src/utilities/passwordChecker.tsx
@@ -14,7 +14,7 @@ export const isPasswordValid = (password: string, passwordConfirmation: string):
             throw "Password too long!"
         }
     
-        const passwordRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&-])[A-Za-z\d@$!%*?&-]{8,60}$/;
+        const passwordRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&^_#()-])[A-Za-z\d@$!%*?&^_#()-]{8,60}$/;
           
         if (!(passwordRegex.test(passwordConfirmation))){
             throw "Password must contain one uppercase letter, one lowercase letter, one digit and a special character and must be at least 8 characters long!"
