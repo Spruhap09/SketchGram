195263b83aba89302f842df15534f1c8ca207f8e
tombyrn <thomasbyrnes02@gmail.com>
2023-12-21 10:50:56 -0500

    added 404 page

---
+++ src/pages/404.tsx
@@ -0,0 +1,7 @@
+import Layout from "@/components/Layout";
+import { Typography } from "@material-tailwind/react";
+export default function Error404() {
+    return <Layout>
+        <Typography variant="h1">Error 404: Resource not found</Typography>
+    </Layout>
+}
